2810a73d10e0b1146a3003d863e8de1432955cb3
vladcaulfield <vladkryzhenkov8@gmail.com>
2024-12-30 02:43:39 +0300

    feat:  add human choice function

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function getComputerChoice() {
 
   return value;
 }
+
+function getHumanChoice() {
+  let choice = prompt("paper? rock? scissors?");
+  if (choice === "paper") {
+    choice = "paper";
+  } else if (choice === "rock") {
+    choice = "rock";
+  } else if (choice === "scissors") {
+    choice = "scissors";
+  } else if (choice === null) {
+    getHumanChoice();
+  } else {
+    let wrongChoice = choice;
+    alert(`${wrongChoice} is NOT what i need. 
+      CHOOSE YOUR FIGHTER`);
+    choice = null;
+  }
+  return choice != null ? choice : getHumanChoice();
+}
