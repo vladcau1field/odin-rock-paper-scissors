e47ebc2a5090a31a51f56f2845d022666c10686e
vladcaulfield <vladkryzhenkov8@gmail.com>
2024-12-31 12:51:08 +0300

    feat:  add increase score function

--- script.js
+++ script.js
@@ -39,26 +39,44 @@ function playRound(humanChoice, computerChoice) {
   // player wins
   if (humanChoice === `paper` && computerChoice === `rock`) {
     console.log(`player win! ${humanChoice} beat ${computerChoice}! `);
-    return humanChoice;
+    increaseScore("player");
+    return "player";
   } else if (humanChoice === `rock` && computerChoice === `scissors`) {
     console.log(`player win! ${humanChoice} beat ${computerChoice}! `);
-    return humanChoice;
+    increaseScore("player");
+    return "player";
   } else if (humanChoice === `scissors` && computerChoice === `paper`) {
     console.log(`player win! ${humanChoice} beat ${computerChoice}! `);
-    return humanChoice;
+    increaseScore("player");
+    return "player";
   }
 
   // computer wins
   else if (computerChoice === `paper` && humanChoice === `rock`) {
     console.log(`computer win! ${computerChoice} beat ${humanChoice}! `);
-    return computerChoice;
+    increaseScore("computer");
+    return "computer";
   } else if (computerChoice === `rock` && humanChoice === `scissors`) {
     console.log(`computer win! ${computerChoice} beat ${humanChoice}! `);
-    return computerChoice;
+    increaseScore("computer");
+    return "computer";
   } else if (computerChoice === `scissors` && humanChoice === `paper`) {
     console.log(`computer win! ${computerChoice} beat ${humanChoice}! `);
-    return computerChoice;
+    increaseScore("computer");
+    return "computer";
   } else {
     console.log("damn it! how you did it?");
+  }
+}
+
+function increaseScore(winner) {
+  if (winner === "player") {
+    ++humanScore;
+    return console.log(humanScore);
+  } else if (winner === "computer") {
+    ++computerScore;
+    return console.log(computerScore);
+  } else {
+    return;
   }
 }
