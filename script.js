a22f9a0118f71ef3fd883576e54e300b7aee73dd
vladcaulfield <vladkryzhenkov8@gmail.com>
2025-01-02 03:07:53 +0300

    fix: put all in playgame function

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-let humanScore = 0;
-let computerScore = 0;
-
-// getting computer choice
-function getComputerChoice() {
-  const randomNumber = Math.random();
-  if (randomNumber < 0.33) {
-    return "rock";
-  } else if (randomNumber < 0.66) {
-    return "paper";
-  } else {
-    return "scissors";
-  }
-}
-
-// getting human choice
-function getHumanChoice() {
-  let choice = prompt("rock, paper, or scissors?");
-  choice = choice.toLowerCase();
-  if (choice === "rock" || choice === "paper" || choice === "scissors") {
-    return choice;
-  } else {
-    alert("invalid choice. CHOOSE YOUR FIGHTER");
-    return getHumanChoice();
-  }
-}
-
 function playGame() {
+  // score variables
+  let humanScore = 0;
+  let computerScore = 0;
+
+  // getting computer choice
+  function getComputerChoice() {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+      return "rock";
+    } else if (randomNumber < 0.66) {
+      return "paper";
+    } else {
+      return "scissors";
+    }
+  }
+
+  // getting human choice
+  function getHumanChoice() {
+    let choice = prompt("rock, paper, or scissors?");
+    choice = choice.toLowerCase();
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
+      return choice;
+    } else {
+      alert("invalid choice. CHOOSE YOUR FIGHTER");
+      return getHumanChoice();
+    }
+  }
+
   //logic of round
   function playRound(humanChoice, computerChoice) {
     console.log(`you chose:${humanChoice} `);
@@ -45,9 +46,9 @@ function playGame() {
       console.log(`you lose! ${computerChoice} beats ${humanChoice}`);
       computerScore++;
     }
-    console.log(
-      `damn look at it!  you - ${humanScore}, computer ${computerScore}`
-    );
+    // console.log(
+    //   `damn look at it!  you - ${humanScore}, computer ${computerScore}`
+    // );
   }
 
   //logic of game
@@ -55,3 +56,5 @@ function playGame() {
   const computerSelection = getComputerChoice();
   playRound(humanSelection, computerSelection);
 }
+
+playGame();
